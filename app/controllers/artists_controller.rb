f54c239a0afddf4bff6ae9de925c33ba03025706
Julien Mayer <jalexismayer@gmail.com>
2015-02-26 20:41:31 -0800

    refactor artist controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,13 +7,21 @@ class ArtistsController < ApplicationController
   end
 
   def create
-    # delete pre-existing artists
-    @artists = Artist.all
-    @artists.each do |artist|
-      artist.destroy
+    artist = params['artist']['name']
+
+    def getSimilar(artist)
+      # last fm uri + method parameter expressed as 'package.method' + limit + key
+      uri = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=#{artist}&limit=3&api_key=ddbf408cce195ed82a523b1d395e5c9b&format=json"
+      # simple restclient request, possible formats from api are xml, json
+      response = RestClient.get(uri)
+      # parse json string into json object
+      json = JSON.parse(response)
+      # array of artist hashes
+      artists = json["similarartists"]["artist"]
     end
 
     def getVideo(artist)
+      # format last fm response
       artist = artist.split().join("")
       # youtube api url + weird part and snippet + type [video, playlist, channel] + limit results
       url = "https://www.googleapis.com/youtube/v3/search?part=id%2C+snippet&q=#{artist}&type=video&maxResults=1&key=AIzaSyCztszklccaoguqRDrzz-Xk5OmHJTbLkiE"
@@ -24,20 +32,9 @@ class ArtistsController < ApplicationController
       ytid = jsonYtube["items"][0]["id"]["videoId"]
     end
 
-    def
-    # index into params hash to retrieve form parameter
-    artist = params['artist']['name']
-    # last fm uri + method parameter expressed as 'package.method' + limit + key
-    uri = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=#{artist}&limit=3&api_key=ddbf408cce195ed82a523b1d395e5c9b&format=json"
-    # simple restclient request, possible formats from api are xml, json
-    response = RestClient.get(uri)
-    # parse json string into json object
-    json = JSON.parse(response)
-    # array of artist hashes
-    artists = json["similarartists"]["artist"]
-    @artists = json["similarartists"]["artist"]
+    similarArtists = getSimilar(artist)
     # persisting to database make youtube api request for each similar artist to retrieve a video for each artist
-    artists.each do |artist|
+    similarArtists.each do |artist|
       @artist = Artist.new
       name = artist["name"]
       ytid = getVideo(name)
